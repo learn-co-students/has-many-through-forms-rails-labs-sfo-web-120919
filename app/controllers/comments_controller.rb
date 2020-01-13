ce464c9a0d1fe264be6c0d31962fa5cb2417577e
github username <ingrey1@gmail.com>
2020-01-13 12:12:55 -0800

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,26 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
 
   def create
+    post = Post.find(params[:comment][:post_id])
+    user = User.exists?(id: params[:comment][:user_id]) ? User.find(params[:comment][:user_id]) : User.create(username: params[:comment][:user_id])
+
+   
     comment = Comment.create(comment_params)
+    post.comments << comment
+    user.comments << comment
+    #byebug
     redirect_to comment.post
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
+
+ 
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attr=(attributes)
+      if attributes[:name] != ""
+        category = Category.find_or_create_by(attributes)
+        self.categories << category
+      end
+  end 
+
+
+
 
 end
